f8ee558bd718d47ffc4d825fe8dccfdd434af44a
Vishnu Priya Nallan Chakravarthula <VishnuPriya.NallanChakravarthula@jda.com>
2018-02-18 00:44:38 +0530

    Added request storage perissions; Read files from Camera path and store into a lilst

--- app/src/main/java/lilacapps/example/com/collagemaker/MainActivity.java
+++ app/src/main/java/lilacapps/example/com/collagemaker/MainActivity.java
@@ -1,9 +1,19 @@
 package lilacapps.example.com.collagemaker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
 
 import service.CollageMakerIntentService;
 
@@ -13,12 +23,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+            //ask for permission
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
     }
 
     public void startService(View view) {
 
+        boolean file_flag = false;
         Intent collageIntent = new Intent(this, CollageMakerIntentService.class);
         startService(collageIntent);
 
+        // Verify if the android system has internal / external storage
+
+        // This verifies internal SD card
+        String extStore = System.getenv("EXTERNAL_STORAGE");
+        if (extStore != null) {
+            Log.i("External storage", extStore);
+            //Get path of Camera
+            File cameraPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+            Log.i("Camera path", cameraPath.getAbsolutePath());
+            String[] list = cameraPath.list();
+            if (list != null) {
+                for (String l : list) {
+                    String path_absolute = cameraPath+"/"+l;
+                    File path_absolute_File = new File(path_absolute);
+                    file_flag = path_absolute_File.isDirectory();
+
+                    if(file_flag){
+                            File[] listOfFiles = path_absolute_File.listFiles();
+                            for(File eachFile : listOfFiles){
+                                Log.i("File path",eachFile.getAbsolutePath());
+                            }
+                    }
+                }
+            }
+        }
+        // This verifies external SD card
+        String secStore = System.getenv("SECONDARY_STORAGE");
+        if (secStore != null) {
+            Log.i("Secondary storage", secStore);
+
+            File file = new File(secStore, "/DCIM");
+            File[] listofFiles = file.listFiles();
+            int i = 0;
+            for (File eachFile : listofFiles) {
+                Log.i("File path of item" + (++i), eachFile.getAbsolutePath());
+            }
+        }
     }
 }
--- app/src/main/java/service/CollageMakerIntentService.java
+++ app/src/main/java/service/CollageMakerIntentService.java
@@ -6,8 +6,12 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.view.View;
+
+import com.lopei.collageview.CollageView;
 
 import lilacapps.example.com.collagemaker.MainActivity;
 
@@ -37,5 +41,24 @@ public class CollageMakerIntentService extends IntentService {
         mBuilder.setContentIntent(mPendingIntent);
         notificationManager.notify(COLLAGE_SERVICE_NOTIFICATION_ID,mBuilder.build());
 
+
+        //copied
+//        CollageView collageView = findViewById(R.id.collageView);
+//        collageView.setVisibility(View.VISIBLE);
+//        collageView
+////                        .photoMargin(1)
+////                        .photoPadding(4)
+//                .backgroundColor(Color.WHITE)
+//                .photoFrameColor(Color.BLUE)
+////                        .useFirstAsHeader(true) // makes first photo fit device widtdh and use full line
+////                        .defaultPhotosForLine(6) // sets default photos number for line of photos (can be changed by program at runtime)
+//                .useCards(true) // adds cardview backgrounds to all photos
+//                .maxWidth(100) // will resize images if their side is bigger than 100
+////                        .placeHolder(R.drawable.draw) //adds placeholder resource
+////                        .headerForm(CollageView.ImageForm.IMAGE_FORM_SQUARE) // sets form of image for header (if useFirstAsHeader == true)
+////                        .photosForm(CollageView.ImageForm.IMAGE_FORM_HALF_HEIGHT) //sets form of image for other photos
+//                .loadPhotos(list_of_urls);
+//    }
+//});
     }
 }
